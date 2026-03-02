a711cf6ffa7b8b3a02f071d538006b914099da2f
faizan khan <118836710+faizan6101@users.noreply.github.com>
2026-03-02 15:26:31 +0530

    category validation implementation

--- src/main/java/com/enotes/exception/GlobalException.java
+++ src/main/java/com/enotes/exception/GlobalException.java
@@ -21,16 +21,21 @@ public class GlobalException {
         return new ResponseEntity<ApiResponse>(response, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        Map<String,String> resp = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach((error) -> {
-            String fieldName = error.getField();
-            String message = error.getDefaultMessage();
-            resp.put(fieldName, message);
-        });
-        String message = ex.getBindingResult().getFieldError().getDefaultMessage();
-        ApiResponse apiResponse = new ApiResponse(message, false);
-        return new ResponseEntity<Map<String,String>>(resp, HttpStatus.BAD_REQUEST);
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+//        Map<String,String> resp = new HashMap<>();
+//        ex.getBindingResult().getFieldErrors().forEach((error) -> {
+//            String fieldName = error.getField();
+//            String message = error.getDefaultMessage();
+//            resp.put(fieldName, message);
+//        });
+//        String message = ex.getBindingResult().getFieldError().getDefaultMessage();
+//        ApiResponse apiResponse = new ApiResponse(message, false);
+//        return new ResponseEntity<Map<String,String>>(resp, HttpStatus.BAD_REQUEST);
+//    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?> handleValidationException(ValidationException ex) {
+        return new ResponseEntity<>(ex.getErrors(),HttpStatus.BAD_REQUEST);
     }
 }
